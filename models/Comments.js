7a9af4ee95271ea07a52db922cc500371c1899f1
taliento <davidetaliento@gmail.com>
2015-11-20 15:53:35 +0100

    fixing

--- models/Comments.js
+++ models/Comments.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 
 var CommentSchema = new mongoose.Schema({
 	  body: String,
-		  author: String,
-		  upvotes: {type: Number, default: 0},
-		  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
+		author: String,
+		upvotes: {type: Number, default: 0},
+		post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
 });
 
 CommentSchema.methods.upvote = function(cb) {
--- models/Posts.js
+++ models/Posts.js
@@ -2,14 +2,16 @@ var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
 	  title: String,
-		  link: String,
-		  upvotes: {type: Number, default: 0},
-		  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+		text:String,
+		author:String,
+		link: String,
+		upvotes: {type: Number, default: 0},
+		comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 });
 
 PostSchema.methods.upvote = function(cb) {
 	  this.upvotes += 1;
-		  this.save(cb);
+		this.save(cb);
 };
 
 mongoose.model('Post', PostSchema);
